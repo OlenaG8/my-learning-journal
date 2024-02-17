c2d7a2c061bcb593e10bff2015e38d6626c7802a
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-02-17 22:19:47 +0100

    Added function to render articles and ensured responsiveness

---
+++ data.js
@@ -0,0 +1,45 @@
+export const postsArray = [
+    {
+        photo: `images/books-and-computer.jpg`,
+        publishingDate: "Sept 1, 2022",
+        title: "Blog 1",
+        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+        After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        photo: `images/books-and-computer.jpg`,
+        publishingDate: "Sept 1, 2022",
+        title: "Blog 2",
+        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+        After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        photo: `images/books-and-computer.jpg`,
+        publishingDate: "Sept 1, 2022",
+        title: "Blog 3",
+        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+        After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        photo: `images/books-and-computer.jpg`,
+        publishingDate: "Sept 1, 2022",
+        title: "Blog 4",
+        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+        After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        photo: `images/books-and-computer.jpg`,
+        publishingDate: "Sept 1, 2022",
+        title: "Blog 5",
+        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+        After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        photo: `images/books-and-computer.jpg`,
+        publishingDate: "Sept 1, 2022",
+        title: "Blog 6",
+        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+        After several months of learning in the Frontend Developer Career Path.`
+    },
+
+]
---
+++ index.js
@@ -0,0 +1,18 @@
+import { postsArray } from './data.js'
+
+function getArticleHtml(post) {
+    return `
+    <article>
+        <img class="article-img" src="${post.photo}" alt="Post thumbnail">
+        <span class="article-info">${post.publishingDate}</span>
+        <h2 class="article-title">${post.title}</h2>
+        <p class="article-preview">${post.preview}</p>                
+    </article>
+    `
+}
+
+function render() {
+    document.getElementById("posts-container").innerHTML = postsArray.map(getArticleHtml).join('')
+}
+
+render()
