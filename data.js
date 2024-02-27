868e83c6f32c7b36337cb05457847f028adc306b
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-02-27 23:50:54 +0100

    Updated posts contents

--- data.js
+++ data.js
@@ -1,46 +1,46 @@
 export const postsArray = [
     {
         photo: `images/getting-things-done.jpg`,
-        publishingDate: "Sept 1, 2022",
-        title: "Blog 1",
-        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
-        After several months of learning in the Frontend Developer Career Path.`
+        publishingDate: "Jan 1, 2023",
+        title: "Bug Busters",
+        preview: `It's time to go through the tangled web of frontend bugs and errors. 
+        Now I'm working on strategies for effective debugging, like using browser developer tools to employing console.log statements.`
     },
     {
         photo: `images/work-hard.jpg`,
-        publishingDate: "Sept 1, 2022",
-        title: "Blog 2",
-        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
-        After several months of learning in the Frontend Developer Career Path.`
+        publishingDate: "Dec 15, 2022",
+        title: "The Art of UI/UX Design",
+        preview: `Join me on a quest to merge code and creativity as we delve into the principles of UI/UX design. 
+        From wireframing to prototyping, I am about to learn how to craft seamless and intuitive experiences that delight users and elevate our frontend creations.`
     },
     {
         photo: `images/img1.jpg`,
-        publishingDate: "Sept 1, 2022",
-        title: "Blog 3",
-        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
-        After several months of learning in the Frontend Developer Career Path.`
+        publishingDate: "Nov 23, 2022",
+        title: "Mastering Responsive Design",
+        preview: `In today's digital landscape, responsive design is non-negotiable. I'm currently learning how to create websites that adapt seamlessly to various screen sizes and devices, ensuring an optimal user experience for all. 
+        From flexible grids to media queries, there are a lot to master keeping accessibility and usability in mind.`
     },
     {
         photo: `images/books-and-computer.jpg`,
-        publishingDate: "Sept 1, 2022",
-        title: "Blog 4",
-        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
-        After several months of learning in the Frontend Developer Career Path.`
+        publishingDate: "Sept 10, 2022",
+        title: "JavaScript Demystified:",
+        preview: `Follow along as I navigate the dynamic landscape of JavaScript, from basic syntax to advanced concepts. 
+        With each lesson and code snippet, I get an inch closer to mastering the language and unlocking its full potential for frontend magic.`
     },
     {
         photo: `images/colors.jpg`,
-        publishingDate: "Sept 1, 2022",
-        title: "Blog 5",
-        preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
-        After several months of learning in the Frontend Developer Career Path.`
+        publishingDate: "July 8, 2022",
+        title: "Unveiling the World of HTML & CSS",
+        preview: `Its time to dive into the foundational languages of web development - HTML and CSS. 
+        I'm learning how to structure web pages and style them with CSS to create visually appealing designs.`
     },
     {
         photo: `images/computer-with-a-plant.jpg`,
         publishingDate: "Jun 26, 2023",
         title: "My Frontend Journey Kickoff",
         preview: `I'm excited to start a new learning journey, which is Frontend Developer Career Path at Scrimba. 
-        The education includes 12 modules. 
-        All modules are filled with interactive coding challenges aiming to teach their students HTML, CSS, JavaScript, React, UI design and more.`
+        The course includes 12 modules. 
+        They are filled with interactive coding challenges aiming to teach their students HTML, CSS, JavaScript, React, UI design and more.`
     },
 
 ]
