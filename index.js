1044bed598d655facedf6215e3fe518eb0929a5c
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-02-29 18:38:25 +0100

    About me section finished

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { postsArray } from './data.js'
+import { postsArray } from './data-posts-array.js'
 
 function getCurrentYear() {
     const date = new Date()
@@ -16,14 +16,40 @@ function getArticleHtml(post) {
     `
 }
 
+function getAboutMeArticle() {
+    return `
+    <h5>How I stay commited to learning</h5>
+    <p>My current team leader once said that I come across as someone who is always eager to learn and explore new things.
+    I believe these words describe my personality the most accurately.
+    <br><br>
+    Since I was a kid, I've always been into a bunch of stuff - drawing, sewing, crocheting, even messing around with clay. 
+    As I got older, I got into physics, math, 3D design, and all sorts of other things. 
+    I had several approaches to programming at different ages, but I never took it seriously because I was influenced by many stereotypes.
+    <br><br>
+    To wrap up, I've never felt the need for external motivation to learn because it's simply a part of who I am.
+    </p>
+    <h5>How I got into programming</h5>
+    <p>Two years ago, I transitioned into a full-time role at an excellent company with a supportive team and a positive work culture. 
+    However, outside of work hours, I found myself bored and struggling to find meaningful ways to invest my time into.
+    <br><br>
+    Afterwards, through a curious twist of fate, I crossed paths with someone at a coffee shop. 
+    This individual shared their journey as a website architect, highlighting how despite lacking formal education, they were recognized as an expert and regularly invited to speak at seminars worldwide.
+    <br><br>
+    Their story inspired me, so I felt the need to give it a shot myself.
+    </p>
+    `
+}
+
 function render() {
     document.getElementById("posts-container").innerHTML += postsArray.map(getArticleHtml).join('')
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('about-me-article').innerHTML = getAboutMeArticle()
     for (let i = 0; i < 3; i++) {
         document.getElementById("recent-posts-container").innerHTML += getArticleHtml(postsArray[i])
     }
+    getCurrentYear()
 })
 
 render()
